1980805c05ba68c7299e511c38c1ece1f6f07c20
jupei <jupei.lu@innocellence.com>
2021-02-03 16:37:53 +0800

    webpack config

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="div1">{{txt}}</div>
+    <img src="../assets/design.png" alt="png">
+  </div>
+</template>
+
+<style>
+  div {
+      color: red;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        txt: '这是App.vue',
+      };
+    },
+    methods: {
+      sum(a, b){
+        return a + b;
+      }
+    },
+    created() {
+      console.log(this.sum(1, 5));
+    }
+  };
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+// {
+//   loader: 'file-loader',
+//     options: {
+//   name: '[hash].[ext]',
+//     outputPath: 'images/',
+//     esModule: false,
+// },
+// },
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/[name].[hash:7].[ext]',
+              limit: 8192,
+              publicPath: './',
+              esModule: false
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  mode: 'development',
+};
